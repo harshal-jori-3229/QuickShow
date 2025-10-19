81a336060679329334f2fcd4641043bafd9fe82a
Harshal Jori <harshaljori3229@gmail.com>
2025-10-19 10:19:21 +0530

    Initial commit - MERN project upload

---
+++ client/src/components/BlurCircle.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function BlurCircle( { top = "auto", left = "auto", right = "auto", bottom = "auto" }) {
+  return (
+    <div
+      className="absolute -z-50 h-56 w-56 aspect-square rounded-full bg-primary/30 blur-3xl"
+      style={{ top: top, left: left, right: right, bottom:bottom }}>
+
+    </div>
+  )
+}
+
+export default BlurCircle
