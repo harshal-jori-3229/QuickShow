b11832cd746e20bd81a81f5a5e19111501464e84
Harshal Jori <harshaljori3229@gmail.com>
2025-10-23 09:42:45 +0530

    initial changes

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -8,7 +8,7 @@ export const inngest = new Inngest({ id: "movie-ticket-booking" });
 //Inngest function to save user data to a database 
 const synUserCreation = inngest.createFunction(
     {id: 'sync-user-from-clerk'},
-    {event: 'clerk/user/created'},
+    {event: 'clerk/user.created'},
     async ({event}) => {
         const {id, first_name, last_name, email_addresses, image_url} = event.data
         const userData ={
