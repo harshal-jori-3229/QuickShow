a7bd24eb16e15bfbe66d2c9bb48c1bd44a8b5703
Harshal Jori <harshaljori3229@gmail.com>
2025-10-27 15:15:20 +0530

    new db changes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-
 import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
