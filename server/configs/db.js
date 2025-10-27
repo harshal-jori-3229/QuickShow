99535fbd10867bd1d484d582c7be6b2c40781a5e
Harshal Jori <harshaljori3229@gmail.com>
2025-10-27 15:52:23 +0530

    db changes

--- server/configs/db.js
+++ server/configs/db.js
@@ -5,7 +5,7 @@ const connectDB = async ()=> {
         mongoose.connection.on('connected', ()=> console.log('Database Connected'));
         await mongoose.connect(`${process.env.MONGODB_URI}/QuickShow`);
     } catch (error){
-        console.log(error.message)
+         console.log("❌ Database Connection Error:", error.message);
     }
 }
 
