6abc612bcee64047cea95e244be3e30604af4274
Harshal Jori <harshaljori3229@gmail.com>
2025-10-27 16:53:49 +0530

    last changes

--- server/configs/db.js
+++ server/configs/db.js
@@ -5,7 +5,7 @@ const connectDB = async ()=> {
         mongoose.connection.on('connected', ()=> console.log('Database Connected'));
         await mongoose.connect(`${process.env.MONGODB_URI}/QuickShow`);
     } catch (error){
-         console.log("❌ Database Connection Error:", error.message);
+        console.log(error.message)
     }
 }
 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import 'dotenv/config';
 import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
