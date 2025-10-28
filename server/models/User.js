4da48238e763cb16f1f4e920f19387f0c9154d8e
Harshal Jori <harshaljori3229@gmail.com>
2025-10-28 11:03:12 +0530

    user schema changes

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,25 @@
+// import mongoose from "mongoose";
+
+// const userSchema = new mongoose.Schema({
+//     _id: {type: String, required: true},
+//     name: {type: String, required: true},
+//     email: {type: String, required: true},
+//     image: {type: String, required: true}
+// })
+
+// const User = mongoose.model('User', userSchema)
+// export default User;
+
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    _id: {type: String, required: true},
-    name: {type: String, required: true},
-    email: {type: String, required: true},
-    image: {type: String, required: true}
-})
+  _id: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  image: { type: String, required: true },
+});
 
-const User = mongoose.model('User', userSchema)
+// Prevent model overwrite issue in dev
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
 export default User;
